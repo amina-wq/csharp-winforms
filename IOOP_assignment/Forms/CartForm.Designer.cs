48842cb5436b03b65b0a1fffa6ddd04b815a521e
Amina <kannami2004@gmail.com>
2024-06-04 00:20:53 +0800

    Changed cart appearance

--- IOOP_assignment/Forms/CartForm.Designer.cs
+++ IOOP_assignment/Forms/CartForm.Designer.cs
@@ -26,16 +26,20 @@
             this.btnCheckout = new System.Windows.Forms.Button();
             this.lblTotal = new System.Windows.Forms.Label();
             this.btnClose = new System.Windows.Forms.Button();
+            this.cartLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.divider = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // flowLayoutPanelCartItems
             // 
             this.flowLayoutPanelCartItems.AutoScroll = true;
             this.flowLayoutPanelCartItems.BackColor = System.Drawing.Color.Transparent;
-            this.flowLayoutPanelCartItems.Location = new System.Drawing.Point(16, 49);
-            this.flowLayoutPanelCartItems.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.flowLayoutPanelCartItems.Location = new System.Drawing.Point(16, 70);
+            this.flowLayoutPanelCartItems.Margin = new System.Windows.Forms.Padding(4);
             this.flowLayoutPanelCartItems.Name = "flowLayoutPanelCartItems";
-            this.flowLayoutPanelCartItems.Size = new System.Drawing.Size(692, 369);
+            this.flowLayoutPanelCartItems.Size = new System.Drawing.Size(692, 348);
             this.flowLayoutPanelCartItems.TabIndex = 0;
             // 
             // btnCheckout
@@ -45,7 +49,7 @@
             this.btnCheckout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCheckout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
             this.btnCheckout.Location = new System.Drawing.Point(548, 426);
-            this.btnCheckout.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCheckout.Margin = new System.Windows.Forms.Padding(4);
             this.btnCheckout.Name = "btnCheckout";
             this.btnCheckout.Size = new System.Drawing.Size(160, 49);
             this.btnCheckout.TabIndex = 1;
@@ -71,7 +75,7 @@
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
             this.btnClose.Location = new System.Drawing.Point(655, 13);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(53, 49);
             this.btnClose.TabIndex = 3;
@@ -79,24 +83,64 @@
             this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
+            // cartLabel
+            // 
+            this.cartLabel.AutoSize = true;
+            this.cartLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.cartLabel.Location = new System.Drawing.Point(312, 9);
+            this.cartLabel.Name = "cartLabel";
+            this.cartLabel.Size = new System.Drawing.Size(81, 39);
+            this.cartLabel.TabIndex = 4;
+            this.cartLabel.Text = "Cart";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::IOOP_assignment.Properties.Resources.restaurants_at_the_corners_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(156, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // divider
+            // 
+            this.divider.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.divider.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.divider.Location = new System.Drawing.Point(16, 416);
+            this.divider.Name = "divider";
+            this.divider.Size = new System.Drawing.Size(692, 2);
+            this.divider.TabIndex = 6;
+            this.divider.BackColor = System.Drawing.SystemColors.ControlDark;
+            // 
             // CartForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(206)))), ((int)(((byte)(170)))));
             this.ClientSize = new System.Drawing.Size(724, 492);
+            this.Controls.Add(this.divider);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cartLabel);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.lblTotal);
             this.Controls.Add(this.btnCheckout);
             this.Controls.Add(this.flowLayoutPanelCartItems);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "CartForm";
             this.Text = "Shopping Cart";
             this.Load += new System.EventHandler(this.CartForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label cartLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label divider;
     }
 }
